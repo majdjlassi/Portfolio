d50127e31eda505d1cf84ae29e8445848420ed25
majdjlassi <majd.jlassi@midge-medical.com>
2023-05-27 01:27:01 +0200

    adjust ui

--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -147,102 +147,113 @@ class _HomePageState extends State<HomePage>
           ),
           SizedBox(
             width: 0.78.sw,
+            height: 0.62.sh,
             child: BlocBuilder<PersonalInfoCubit, PersonalInfoState>(
               builder: (context, state) => Padding(
                 padding: const EdgeInsets.only(right: 2.0),
                 child: switch (state) {
                   PersonalInfoStateLoading() => Container(),
-                  PersonalInfoStateSuccess() => Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 32.h,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16.w),
-                          child: Row(
-                            children: [
-                              SvgPicture.asset(
-                                'assets/images/ic_coding.svg',
-                              ),
-                              SizedBox(
-                                width: 16.w,
-                              ),
-                              Flexible(
-                                child: Text(
-                                  state.data.position,
-                                  style: context.sBodyLarge!.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    letterSpacing: 2.0,
+                  PersonalInfoStateSuccess() => RawScrollbar(
+                      thumbVisibility: true,
+                      thumbColor: context.backgroundColor,
+                      radius: Radius.circular(12.w),
+                      thickness: 3,
+                      padding:
+                          EdgeInsets.only(top: 32.h, bottom: 32.h, right: 4.w),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 32.h,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 16.w),
+                              child: Row(
+                                children: [
+                                  SvgPicture.asset(
+                                    'assets/images/ic_coding.svg',
                                   ),
-                                ),
+                                  SizedBox(
+                                    width: 16.w,
+                                  ),
+                                  Flexible(
+                                    child: Text(
+                                      state.data.position,
+                                      style: context.sBodyLarge!.copyWith(
+                                        fontWeight: FontWeight.bold,
+                                        letterSpacing: 2.0,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
+                            ),
+                            SizedBox(
+                              height: 24.h,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 16.w),
+                              child: Text(
+                                state.data.bio,
+                                textAlign: TextAlign.justify,
+                                style: context.sBodyLarge,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 32.h,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 16.w),
+                              child: Text(
+                                'Contact',
+                                style: context.pHeadlineSmall!
+                                    .copyWith(color: context.backgroundColor),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 24.h,
+                            ),
+                            LinkTile(
+                              svgAsset: 'assets/images/ic_email.svg',
+                              label: state.data.email,
+                              onItemClick: () {
+                                launchUrl(Uri(
+                                    scheme: 'mailto', path: state.data.email));
+                              },
+                            ),
+                            LinkTile(
+                              svgAsset: 'assets/images/ic_address.svg',
+                              label: state.data.address,
+                              onItemClick: () {
+                                _openMapWithAddress(state.data.address);
+                              },
+                            ),
+                            LinkTile(
+                              svgAsset: 'assets/images/ic_mobile.svg',
+                              label: state.data.number,
+                              onItemClick: () {
+                                launchUrl(Uri(
+                                    scheme: 'tel', path: state.data.number));
+                              },
+                            ),
+                            LinkTile(
+                              svgAsset: 'assets/images/ic_github.svg',
+                              label: 'Github',
+                              onItemClick: () {
+                                launchUrlString(state.data.githubUrl);
+                              },
+                            ),
+                            LinkTile(
+                              svgAsset: 'assets/images/ic_linkedin.svg',
+                              label: 'Linkedin',
+                              onItemClick: () {
+                                launchUrlString(state.data.linkedinUrl);
+                              },
+                            ),
+                          ],
                         ),
-                        SizedBox(
-                          height: 24.h,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16.w),
-                          child: Text(
-                            state.data.bio,
-                            textAlign: TextAlign.justify,
-                            style: context.sBodyLarge,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 32.h,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 16.w),
-                          child: Text(
-                            'Contact',
-                            style: context.pHeadlineSmall!
-                                .copyWith(color: context.backgroundColor),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 24.h,
-                        ),
-                        LinkTile(
-                          svgAsset: 'assets/images/ic_email.svg',
-                          label: state.data.email,
-                          onItemClick: () {
-                            launchUrl(
-                                Uri(scheme: 'mailto', path: state.data.email));
-                          },
-                        ),
-                        LinkTile(
-                          svgAsset: 'assets/images/ic_address.svg',
-                          label: state.data.address,
-                          onItemClick: () {
-                            _openMapWithAddress(state.data.address);
-                          },
-                        ),
-                        LinkTile(
-                          svgAsset: 'assets/images/ic_mobile.svg',
-                          label: state.data.number,
-                          onItemClick: () {
-                            launchUrl(
-                                Uri(scheme: 'tel', path: state.data.number));
-                          },
-                        ),
-                        LinkTile(
-                          svgAsset: 'assets/images/ic_github.svg',
-                          label: 'Github',
-                          onItemClick: () {
-                            launchUrlString(state.data.githubUrl);
-                          },
-                        ),
-                        LinkTile(
-                          svgAsset: 'assets/images/ic_linkedin.svg',
-                          label: 'Linkedin',
-                          onItemClick: () {
-                            launchUrlString(state.data.linkedinUrl);
-                          },
-                        ),
-                      ],
+                      ),
                     ),
                   PersonalInfoStateFailed() => Container(),
                 },
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter/services.dart';
+import 'package:flutter_cache_manager/file.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:intl/intl.dart';
 import 'package:portfolio/data/model/experience.dart';
 
@@ -21,4 +23,13 @@ String convertDate(String date) {
   DateTime parsedDate = inputFormat.parse(date);
   final DateFormat formatter = DateFormat('MMM yyyy');
   return formatter.format(parsedDate);
+}
+
+Future<File> getOrDownloadImage(String imageUrl) async {
+  final cacheFile = await DefaultCacheManager().getFileFromCache(imageUrl);
+  if (cacheFile != null) {
+    return cacheFile.file;
+  }
+  return (await DefaultCacheManager().downloadFile(imageUrl, key: imageUrl))
+      .file;
 }
